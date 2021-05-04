24b5414d5f67e2c0d9e1eb77f4de4ec1b58f1baa
Toni Virtanen <toniKoodaa@gmail.com>
2021-05-04 21:23:28 +0300

    clear button

--- index.js
+++ index.js
@@ -5,19 +5,30 @@ Array.prototype.sample = function() {
 const styles = ['Upsi', 'Antsa', 'Hysse', 'Backhand', 'Fore'];
 const discs = ['Putteri', 'Midari', 'Väylädriver', 'Pituusdriver'];
 
-const btn = document.querySelector('#next');
+const btnNext = document.querySelector('#next');
+const btnClear = document.querySelector('#clear');
 const ulElm = document.querySelector('#throws');
 
-btn.innerText = "Next throw";
-btn.addEventListener("click", setRandomThrow)
+btnNext.innerText = "Next";
+btnNext.addEventListener("click", setRandomThrow)
 
+btnClear.innerText = "Clear";
+btnClear.addEventListener("click", clearThrows)
 
+function clearThrows(event) {
+    event.preventDefault();
+    while(ulElm.firstChild) {
+        ulElm.removeChild(ulElm.firstChild);
+    }
+}
 
 function setRandomThrow(event) {
     event.preventDefault();
     const randomizedShot = `Heittotyyli: ${styles.sample()} --- kiekko: ${discs.sample()}`
     
     let li = document.createElement('li');
+    li.setAttribute('class', 'item');
     li.appendChild(document.createTextNode(randomizedShot));
+
     ulElm.appendChild(li);
 }
